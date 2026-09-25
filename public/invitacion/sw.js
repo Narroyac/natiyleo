bd06d7e2d98a8f26d143c07d03f04b75327c97f4
NarroyacWK <narroyave@winterkpital.com>
2026-09-25 00:29:28 -0500

    chore: subir versión del service worker

--- public/invitacion/sw.js
+++ public/invitacion/sw.js
@@ -3,7 +3,7 @@
 // abra rápido e instalada como PWA. Los datos (Supabase) siempre van por red:
 // este SW nunca intercepta peticiones a otros orígenes.
 
-const CACHE_VERSION = "pasaporte-shell-v38";
+const CACHE_VERSION = "pasaporte-shell-v39";
 
 const SHELL_FILES = [
   "./index.html",
