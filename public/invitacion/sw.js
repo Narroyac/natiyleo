53f926f1d38982af8dbecbe70650c20ae45651a7
NarroyacWK <narroyave@winterkpital.com>
2026-09-24 22:18:08 -0500

    chore: subir versión del service worker tras rediseño de motion de sellado

--- public/invitacion/sw.js
+++ public/invitacion/sw.js
@@ -3,7 +3,7 @@
 // abra rápido e instalada como PWA. Los datos (Supabase) siempre van por red:
 // este SW nunca intercepta peticiones a otros orígenes.
 
-const CACHE_VERSION = "pasaporte-shell-v32";
+const CACHE_VERSION = "pasaporte-shell-v33";
 
 const SHELL_FILES = [
   "./index.html",
