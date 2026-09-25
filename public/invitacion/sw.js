2b6e5318a871671079e6c9a327f40478131c3b88
NarroyacWK <narroyave@winterkpital.com>
2026-09-24 23:40:22 -0500

    chore: subir versión del service worker tras el fix de crossfade

--- public/invitacion/sw.js
+++ public/invitacion/sw.js
@@ -3,7 +3,7 @@
 // abra rápido e instalada como PWA. Los datos (Supabase) siempre van por red:
 // este SW nunca intercepta peticiones a otros orígenes.
 
-const CACHE_VERSION = "pasaporte-shell-v34";
+const CACHE_VERSION = "pasaporte-shell-v35";
 
 const SHELL_FILES = [
   "./index.html",
